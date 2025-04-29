2089cd7634b921ace3f8b339aea8871bd462b683
Amir Rajan <ar@amirrajan.net>
2025-04-29 11:39:41 -0500

    untabbify file

--- init-editor.el
+++ init-editor.el
@@ -50,15 +50,15 @@
 
   ;; remove extrenous info in file explorer
   (add-hook 'dired-mode-hook
-	    (lambda ()
-	      (dired-hide-details-mode))))
+            (lambda ()
+              (dired-hide-details-mode))))
 
 ;; fancy themes for emacs
 (use-package doom-themes
   :ensure t
   :config
   ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+  (setq doom-themes-enable-bold t ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
 
   (load-theme 'doom-one t)
